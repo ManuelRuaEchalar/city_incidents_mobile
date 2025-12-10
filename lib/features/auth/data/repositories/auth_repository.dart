1204944d12160ba321ccdde2bb8a8bf3d40a231d
ManuelRua <jruaechalar@gmal.com>
2025-12-10 16:36:05 -0400

    auth UI error handling

--- lib/features/auth/data/repositories/auth_repository.dart
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -9,21 +9,24 @@ class AuthRepository {
   final String baseUrl = AppStrings.baseUrl;
 
   Future<LoginResponse> signIn(LoginRequest request) async {
+    final http.Response response;
     try {
-      final response = await http.post(
+      response = await http.post(
         Uri.parse('$baseUrl/auth/signin'),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode(request.toJson()),
       );
-
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        final jsonResponse = jsonDecode(response.body);
-        return LoginResponse.fromJson(jsonResponse);
-      } else {
-        throw Exception('Error al iniciar sesión: ${response.statusCode}');
-      }
     } catch (e) {
       throw Exception('Error de conexión: $e');
+    }
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      final jsonResponse = jsonDecode(response.body);
+      return LoginResponse.fromJson(jsonResponse);
+    } else if (response.statusCode == 403) {
+      throw Exception('Email o contraseña incorrectos, intentar de nuevo');
+    } else {
+      throw Exception('Error al iniciar sesión: ${response.statusCode}');
     }
   }
 
